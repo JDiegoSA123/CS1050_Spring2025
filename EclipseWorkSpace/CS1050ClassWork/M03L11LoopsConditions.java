ce793e3507bc0829ddfd96e2681fef9b0a0c2acd
JDiegoSA123 <diegololstar123@gmail.com>
2025-03-03 16:18:42 -0700

    Create M03L11LoopsConditions.java

---
+++ EclipseWorkSpace/CS1050ClassWork/M03L11LoopsConditions.java
@@ -0,0 +1,21 @@
+
+public class M03L11LoopsConditions {
+
+	
+	
+	public static void main(String[] args) {
+		
+	final int MAX = 10; 
+	int sum;
+		for (int x = 1; x < MAX; x++) 
+		{
+			
+			sum = (x + x);
+			
+		}// end of for loop
+		
+		System.out.println("The sum of x is " + sum);
+		
+	}// end of main method
+
+}// end of class
