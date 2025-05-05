52525202b57254e7ca0c83f2522934d2da275c9f
JDiegoSA123 <diegololstar123@gmail.com>
2025-05-05 17:22:25 -0600

    99% Done with Project 2

--- EclipseWorkSpace/CS1050ClassWork/ProjectIteration02.java
+++ EclipseWorkSpace/CS1050ClassWork/ProjectIteration02.java
@@ -10,9 +10,9 @@ public class ProjectIteration02
 {
 	public static void main(String[] args) 
 	{
-	double[] athlete1Calories = {100, 200, 300, 400, 500, 600, 700};
-	Athlete athlete1 = new Athlete("Diego", "Sanchez", 150, 68, 21, athlete1Calories);
-	
+	File athletes1File = new File("gym1.txt");
+	File athletes2File = new File("gym2.txt");
+
 	final int MAX_GYM_MEMBERS = 3;
 	
 	Gym gym1 = new Gym("Elite Fitness", MAX_GYM_MEMBERS);
@@ -20,8 +20,8 @@ public class ProjectIteration02
 	try
 	{
 		readGymAthletesInfoFromFile("gym1.txt", gym1);
-		gym1.displayAthleteSummaries();
-		gym1.saveReportToFile();
+		gym1.displayAthleteSummary();
+		gym1.saveReportToFile(athletes1File);
 		
 	} catch (FileNotFoundException e)
 	{
@@ -33,8 +33,8 @@ public class ProjectIteration02
 	try
 	{
 		readGymAthletesInfoFromFile("gym2.txt", gym2);
-		gym2.displayAthleteSummaries();
-		gym2.saveReportToFile();
+		gym2.displayAthleteSummary();
+		gym2.saveReportToFile(athletes2File);
 		
 	} catch (FileNotFoundException e)
 	{
@@ -46,7 +46,66 @@ public class ProjectIteration02
 	
 	}//End of Main Method
 	
+	public static void readGymAthletesInfoFromFile(String filename, Gym currentGym) throws FileNotFoundException
+	{
+		Scanner fileScanner = null;
+		try
+		{
+
+			fileScanner = new Scanner(new File(filename));
+	         
+
+	            while (fileScanner.hasNextLine()) {
+	                String line = fileScanner.nextLine();
+	                System.out.println(line);
+	            }
+
+	            fileScanner.close();
+	    }
+		catch (FileNotFoundException e) 
+			{
+	            System.out.println("Error: Could not open file: " + e.getMessage());
+	        }
+
+
+		finally
+		{
+			if (fileScanner != null)
+			{
+				fileScanner.close();
+			}
+		}
+	}// end add athletes
 	
+	public static void gymSetUp(Gym gym, String filename) throws FileNotFoundException
+	{
+		Scanner fileScanner = null;
+		try
+		{
+			fileScanner = new Scanner(new File(filename));
+			while (fileScanner.hasNextLine())
+			{
+				double [] dailyCaloriesBurned = new double[7];
+				String firstName = fileScanner.next().trim();
+				String lastName = fileScanner.next().trim();
+				double height = fileScanner.nextDouble();
+				double weight = fileScanner.nextDouble();
+				int age = fileScanner.nextInt();
+				for(int i = 0; i < dailyCaloriesBurned.length; i++)
+				{
+					dailyCaloriesBurned[i] = fileScanner.nextDouble();
+				}
+				Athlete currentAthlete = new Athlete(firstName, lastName, weight, height, age, dailyCaloriesBurned);
+				gym.addAthlete(currentAthlete);
+			}
+		} finally
+		{
+			if (fileScanner != null)
+			{
+				fileScanner.close();
+			}
+		}
+	}
 }// End of Class
 
 class Athlete
@@ -68,7 +127,7 @@ class Athlete
 	this.weight = weight;
 	this.age = age;
 	this.dailyCaloriesBurned = dailyCaloriesBurned;
-	}// End of Athlete Method
+	}// End of Athlete Constructor
 	
 	public String getFirstName() 
 	{
@@ -124,72 +183,78 @@ class Athlete
 		return avgCalories;
 	}// End of getAverageCaloriesBurned
 	
-	public static void readGymAthletesInfoFromFile(String filename, Gym currentGym) throws FileNotFoundException
-	{
-		
-		
-		Scanner fileScanner = null;
-		try
-		{
-
-			fileScanner = new Scanner(new File(filename));
-
-			final String FILENAME = "gym1.txt"; 
-
-	        	File testFile = new File(FILENAME);
-	         
-
-	            while (fileScanner.hasNextLine()) {
-	                String line = fileScanner.nextLine();
-	                System.out.println(line);
-	            }
-
-	            fileScanner.close();
-	    }
-		catch (FileNotFoundException e) 
-			{
-	            System.out.println("Error: Could not open file: " + e.getMessage());
-	        }
-
-
-		finally
-		{
-			if (fileScanner != null)
-			{
-				fileScanner.close();
-			}
-		}
-	}// end add athletes
-
-
 
 }// End of Athlete Class
+
 
 class Gym
 {
 private String name;
 private Athlete[] athletes;
-private int numAthletes;
+private int maxNumAthletes;
+private int currentNumAthletes;
 
-	public Gym(String name, int numAthletes)
+
+	public Gym(String name, int maxNumAthletes)
 	{
 		this.name = name;
-		this.numAthletes = numAthletes;
-	}// End of Gym Method
+		this.athletes = new Athlete[maxNumAthletes];
+		this.currentNumAthletes = 0;
+	}// End of Gym Constructor
 
 	public void addAthlete(Athlete athlete)
 	{
+		if (maxNumAthletes < athletes.length)
+		{
+			athletes[maxNumAthletes] = athlete;
+			currentNumAthletes++;
+		} else
+		
+			System.out.println("Gym is full. Couldn't add : " + athlete.getFirstName() + " " + athlete.getLastName());
+		
 	
 	}// End of addAthlete Method
 
 	public void displayAthleteSummary()
 	{
+		System.out.println("***** Gym Weekly Fitness Summary *****");
+		System.out.println("Gym: " + name);
+
+		for(int i = 0; i < athletes.length; i++)
+		{
+			System.out.println("Athlete: "+ athletes[i].getFirstName() + " " + athletes[i].getLastName() + "\n" + "	Max Heart Rate: "+ athletes[i].calculateMaxHeartRate() + "\n"
+							   + "	Average Daily Calories Burned: " + athletes[i].getAverageCaloriesBurned() + "\n");
+			System.out.printf("	BMI: %.1f",  athletes[i].calculateBMI());
+			System.out.println("	Category: " + athletes[i].getBMICategory());
+		}
+		
+		System.out.println("Top Athlete (Most Average Daily Calories Burned): " + (findTopAthlete() + 1));
 	
+		System.out.println("\nUnderweight athletes: ");
+		displayUnderweightAthletes();
+		
+		int counter = 0;
+		for (int i = 0; i < athletes.length; i++)
+		{
+		if( athletes[i].calculateBMI() < 18.5)
+		{
+			counter++;
+		}
+		
+		}
+		if(counter == 0)
+		{
+			System.out.println("No underweight athletes");
+		}
+		
 	}// End of displayAthleteSummary Method
 
-	public void saveReportToFile()
+	public void saveReportToFile(File testFile)
 	{
 	
+		String absoluteFilePath = testFile.getAbsolutePath();
+		System.out.println("\nReport Saved to: " + absoluteFilePath);
+		
 	}// End of saveReportToFile Method
 
 	public int findTopAthlete()
@@ -198,7 +263,7 @@ private int numAthletes;
 		
 		for (int i = 1; i < athletes.length; i++)
 		{
-			if (athletes[i] > athletes[largestIndex]) 
+			if (athletes[i].getAverageCaloriesBurned() > athletes[largestIndex].getAverageCaloriesBurned()) 
 			{
 				largestIndex = i;
 			}
@@ -208,53 +273,18 @@ private int numAthletes;
 
 	public void displayUnderweightAthletes()
 	{
-		
-	}// End of displayUnderweightAthletes Method
-	
-	public static void readGymAthletesInfoFromFile(String filename, Gym currentGym) throws FileNotFoundException
-	{
-		
-		
-		Scanner fileScanner = null;
-		try
+		for(int i = 1; i < athletes.length; i++)
 		{
-
-			fileScanner = new Scanner(new File(filename));
-
-			final String FILENAME = "gym1.txt"; 
-
-	        	File testFile = new File(FILENAME);
-	         
-
-	            while (fileScanner.hasNextLine()) {
-	                String line = fileScanner.nextLine();
-	                System.out.println(line);
-	            }
-
-	            fileScanner.close();
-	    }
-		catch (FileNotFoundException e) 
+			if (athletes[i].calculateBMI() < 18.5)
 			{
-	            System.out.println("Error: Could not open file: " + e.getMessage());
-	        }
-
-
-		finally
-		{
-			if (fileScanner != null)
-			{
-				fileScanner.close();
+				System.out.println("Athlete " + (i + 1));
 			}
 		}
-	}// end add athletes
+	}// End of displayUnderweightAthletes Method
+	
+
 
 }// End of Gym Class
-
-
-
-
-
-
 
 
 
