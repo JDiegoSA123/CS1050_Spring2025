f6372b86f23d3d7ffca106fbe037d4521aaa9443
JDiegoSA123 <diegololstar123@gmail.com>
2025-09-02 17:35:10 -0600

    Create libraryTest.java

---
+++ EclipseWorkSpace/CS2050ClassExamples/libraryTest.java
@@ -0,0 +1,40 @@
+public class libraryTest 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	Book book1 = new Book("BookA ", "Diego ", 1997);
+	
+	System.out.print(book1.getName());
+	System.out.print(book1.getAuthor());
+	System.out.print(book1.getYear());
+	} // End of Main Method
+	
+} // End of libraryTest Class	
+
+class Book
+{
+	private String name;
+	private String author;
+	private int year;
+	
+	public Book(String name, String author, int year)
+	{
+		this.name = name;
+		this.author = author;
+		this.year = year;
+	} // End of Book Constructor
+	public String getName()
+	{
+		return name;
+	}
+	public String getAuthor()
+	{
+		return author;
+	}
+	public int getYear()
+	{
+		return year;
+	}
+} // End of Book Class
